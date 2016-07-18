a7e7e9922f6069a11c6f48cacecb737cc6dcd810
Murray Christopherson <murraychristopherson@gmail.com>
2016-07-18 14:50:30 -0400

    First pass at adding the bottle server.

---
+++ sqlschema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "available_restaurants" (
+	`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`name` TEXT NOT NULL,
+	`menu_url` TEXT DEFAULT NULL
+);
+
+CREATE TABLE "orders" (
+	`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`ordered_at` INT NOT NULL,
+	`ordered_by` TEXT NOT NULL,
+	`order_text` TEXT NOT NULL
+);
+CREATE INDEX "orders_ordered_at_idx" ON "orders" (`ordered_at`);
