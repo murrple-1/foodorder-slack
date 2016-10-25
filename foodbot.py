5e607e188887932922f141b781def5323f899b1f
Murray Christopherson <murraychristopherson@gmail.com>
2016-10-25 17:50:11 -0400

    Setting up the endpoints.

--- foodbot.py
+++ foodbot.py
@@ -2,25 +2,32 @@ import re
 
 from slackbot.bot import respond_to, listen_to, default_reply
 
-@respond_to('hi', re.IGNORECASE)
-def hi(message):
-    message.reply('I can understand hi or HI!')
-    # react with thumb up emoji
-    message.react('+1')
+@respond_to('today\'s menu is (.*)', re.IGNORECASE)
+def set_todays_menu(message, todays_menu_url):
+    message.reply('I\'ve set today\'s menu to \'{0}\''.format(todays_menu_url))
 
-@respond_to('I love you')
-def love(message):
-    message.reply('I love you too!')
+@listen_to('what\'s today\'s menu?', re.IGNORECASE)
+def todays_menu_listen(message):
+    todaysMenu = _todays_menu()
+    message.send(todaysMenu)
 
-@listen_to('Can someone help me?')
-def help(message):
-    # Message is replied to the sender (prefixed with @user)
-    # message.reply('Yes, I can!')
+@respond_to('what\'s today\'s menu?', re.IGNORECASE)
+def todays_menu_respond(message):
+    todaysMenu = _todays_menu()
+    message.reply(todaysMenu)
 
-    # Message is sent on the channel
-    message.send('I can help everybody!')
+def _todays_menu():
+    return 'Today\'s menu is: {0}'.format('McDonald\'s')
 
-@default_reply
+@respond_to('my order: (.*)', re.IGNORECASE)
+def set_order(message, order):
+    message.reply('Thank you for your order')
+
+@respond_to('what are today\'s orders?')
+def todays_orders(message):
+    message.reply('I ordered the fish')
+
+@default_reply()
 def default_reply(message):
-    reply = u'Sorry, but I didn\' understand you\nAvailable commands:\n{0}'.format(message.docs_reply())
+    reply = u'Sorry, but I didn\' understand you'
     message.reply(reply)
