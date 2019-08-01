0d2e68816304e8e3a991fe2338d6cc124166360b
Murray Christopherson <murraychristopherson@gmail.com>
2019-08-01 15:55:26 -0400

    switching from isinstance to type checking (as bool/int is tricksy)

--- foodorder-slackbot/announcethread.py
+++ foodorder-slackbot/announcethread.py
@@ -39,7 +39,7 @@ def start_announce_thread(slackclient):
     will_announce = getattr(settings, 'ANNOUNCE_WILL_LAUNCH', False)
     if will_announce:
         channel_ids = []
-        if isinstance(settings.ANNOUNCE_CHANNEL_NAMES, str):
+        if type(settings.ANNOUNCE_CHANNEL_NAMES) is str:
             channel_id = slackclient.find_channel_by_name(
                 settings.ANNOUNCE_CHANNEL_NAMES)
             if not channel_id:
@@ -58,14 +58,14 @@ def start_announce_thread(slackclient):
                 channel_ids.append(channel_id)
 
         messages = []
-        if isinstance(settings.ANNOUNCE_MESSAGES, str):
+        if type(settings.ANNOUNCE_MESSAGES) is str:
             messages.append(settings.ANNOUNCE_MESSAGES)
         else:
             messages.extend(settings.ANNOUNCE_MESSAGES)
 
         cron_schedule_times = []
         for cron_schedule_time in settings.ANNOUNCE_CRON_SCHEDULE_TIMES:
-            if isinstance(cron_schedule_time, CronScheduleTime):
+            if type(cron_schedule_time) is CronScheduleTime:
                 cron_schedule_times.append(cron_schedule_time)
             else:
                 cron_schedule_times.append(
